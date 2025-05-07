4162b09c4d139400bd3091db24a2eb7cb1eb8c2c
Tomasz Woźniak <wozniak.tom@pm.me>
2025-05-07 21:45:42 +1000

    Create nicekitten.R
    to be developed

---
+++ nicekitten.R
@@ -0,0 +1,14 @@
+
+# pkgKitten::kitten(
+#   name = "nicepackage",
+#   author = "Tomasz Woźniak",
+#   email = "wozniak.tom@pm.me",
+#   license = "GPL-3"
+# )
+
+RcppArmadillo::RcppArmadillo.package.skeleton(
+  "nicepackage", 
+  author="Tomasz Woźniak", 
+  email="wozniak.tom@pm.me", 
+  license = "GPL-3"
+)
